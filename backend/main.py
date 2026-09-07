1f4eb4180caabdbf470fa21cfa01e85c2726a0bb
Vivek18 <viveksaha096@gmail.com>
2026-09-07 08:23:19 +0530

    implement user registration with password hashing

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI, Depends
 from database import engine, Base, get_db
 from sqlalchemy.orm import Session
 import models
-
+import schemas
+import auth
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
@@ -15,3 +16,20 @@ def root():
 @app.get("/test-db")
 def test_db(db: Session = Depends(get_db)):
     return "Database session created successfull"
+
+@app.post("/register")
+def registerUser(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    existing_user = db.query(models.User).filter(
+        models.User.email == user.email
+    ).first()
+    if existing_user:
+        return {"message" : "Email already Registered"}
+
+    hashed_password = auth.hash_password(user.password)
+    new_user = models.User(
+        email=user.email,
+        hashed_password=hashed_password
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
--- backend/schemas.py
+++ backend/schemas.py
@@ -1,1 +1,9 @@
-from pydantic import 
+from pydantic import BaseModel
+
+class UserCreate(BaseModel):
+    email: str
+    password: str
+
+class UserLogin(BaseModel):
+    email: str
+    password: str
