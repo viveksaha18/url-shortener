3c69022f1a27c93fb94f656a660e32f8f2f93a75
Vivek18 <viveksaha096@gmail.com>
2026-09-09 08:50:11 +0530

    Allow Vercel frontend origin

--- backend/main.py
+++ backend/main.py
@@ -21,6 +21,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5174",
+        "https://url-shortener-virid-five.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
